0b6c8ee3c1adfca1c1585a9448d86dffbbf5801a
Jakub Stanczak <kuba0095@gmail.com>
2017-01-05 12:16:01 +0100

    Change main compare to diff

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import sys
-
 import matplotlib.pyplot as plt
-from numpy import *
 from scipy import *
 from scipy.io import wavfile
-from skimage import measure
+
+import sys
 
 
 def recognize_gender(file, freqFilter=1):
@@ -16,7 +14,7 @@ def recognize_gender(file, freqFilter=1):
     if isinstance(signal[0], ndarray):
         signal = [s[0] for s in signal]
 
-    #uprosc sygnal dla szybszych obliczen
+    # uprosc sygnal dla szybszych obliczen
     signal = signal[::freqFilter]
 
     # wyliczanie czestotliwosci sygnalu
@@ -24,15 +22,14 @@ def recognize_gender(file, freqFilter=1):
     drawfftSig = abs(fftSig)
 
     # odciecie prawej symetrycznej strony
-    drawfftSig = drawfftSig[0:len(fftSig)//2]
+    drawfftSig = drawfftSig[0:len(fftSig) // 2]
 
     # licza probek
     n = len(signal)
 
     # wyliczenie x
     freqs = linspace(0, w, n)
-    freqs = freqs[0:len(fftSig)//2]
-
+    freqs = freqs[0:len(fftSig) // 2]
 
     # wyrysowanie wykresow
     fig = plt.figure(figsize=(15, 6), dpi=80)
@@ -43,7 +40,7 @@ def recognize_gender(file, freqFilter=1):
     ax.set_ylim([0.0, 1.1 * max(drawfftSig) * 2 / n])
 
     # zmniejszenie ilosci rysowanych probek
-    drawFirst = int(n/10)
+    drawFirst = int(n / 10)
     drawDiv = 5
     # plt.stem(freqs[:drawFirst:drawDiv], (drawfftSig[:drawFirst:drawDiv] * 2) / n, '-*')
 
@@ -52,38 +49,35 @@ def recognize_gender(file, freqFilter=1):
     # plt.show()
 
 
-def frequencyCheck(signal, freqs, maleMin=0, maleMax=180, femaleMin=165, femaleMax=500, diffSD=100, checkFreqs=300):
-    order = sorted(arange(len(signal)), key = lambda i : signal[i])[-checkFreqs:]
+def frequencyCheck(signal, freqs, freqCheckVal=118, mainFreqFealeVeto=180, checkFreqs=500):
+    order = sorted(arange(len(signal)), key=lambda i: signal[i])[-checkFreqs:]
     freqsToCheck = [freqs[i] for i in order[::-1]]
     # print(freqsToCheck)
 
     mainFreq, diff = findMainFreq(freqsToCheck)
-    if mainFreq < maleMax and mainFreq > femaleMin:
-        if diff < diffSD :
-            return "M"
-        else:
+
+    if diff < freqCheckVal:
+        if mainFreq > mainFreqFealeVeto:
             return "K"
-    if mainFreq > maleMin and mainFreq < maleMax:
         return "M"
-    if mainFreq > femaleMin and mainFreq < femaleMax:
+    else:
         return "K"
 
-    return "IDK"
 
 def findMainFreq(freqs, sd=55):
-    main = {freqs[0] : 0}
+    main = {freqs[0]: 0}
     # dla kazdego elementu na liscie
     for i in range(1, len(freqs)):
         # jezeli jest w main jako key dodaj 1 do count
         inMain = False
         for key, count in main.items():
-            if abs(freqs[i] - key) < sd :
+            if abs(freqs[i] - key) < sd:
                 main[key] = count + 1
                 inMain = True
                 break
 
         # jezeli nie bylo w main jako key dodaj
-        if inMain == False :
+        if inMain == False:
             main[freqs[i]] = 0
 
     main = dict((key, val) for key, val in main.items() if val != 0)
@@ -93,25 +87,38 @@ def findMainFreq(freqs, sd=55):
     print(mainFreq, diff)
     return mainFreq, diff
 
-def calculateDiff(dict, outlierFilter=100):
+
+def calculateDiff(dict):
     sum, count = 0, 0
     keys = list(dict.keys())
     vals = list(dict.values())
     order = sorted(arange(len(keys)), key=lambda i: keys[i])
 
-    for i in order:
-        print(keys[i], vals[i])
+    # for i in order:
+    #     print(keys[i], vals[i])
 
     for i in range(len(order) - 1):
-        diff = abs(keys[order[i]] - keys[order[i+1]])
-        sum += diff * (vals[order[i]] + vals[order[i+1]])
-        count += (vals[order[i]] + vals[order[i+1]])
+        diff = abs(keys[order[i]] - keys[order[i + 1]])
+        sum += diff * (vals[order[i]] + vals[order[i + 1]])
+        count += (vals[order[i]] + vals[order[i + 1]])
 
-    return (sum / count) + min(keys), (sum/count)
+    return min(keys), (sum / count)
+
 
 def main():
     print(sys.argv)
-    recognize_gender("resources/010_M.wav", freqFilter=1)
+
+    files = ["resources/002_M.wav", "resources/003_K.wav", "resources/004_M.wav", "resources/005_M.wav",
+             "resources/006_K.wav", "resources/007_M.wav", "resources/008_K.wav", "resources/009_K.wav",
+             "resources/010_M.wav", "resources/011_M.wav"]
+
+    files2 = ["resources/012_K.wav", "resources/013_M.wav", "resources/014_K.wav", "resources/015_K.wav",
+              "resources/016_K.wav", "resources/017_M.wav", "resources/018_K.wav", "resources/019_M.wav",
+              "resources/020_M.wav", "resources/021_M.wav"]
+
+    for i in files:
+        recognize_gender(i, freqFilter=1)
+
 
 if __name__ == "__main__":
     main()
