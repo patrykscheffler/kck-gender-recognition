3549c3c0e8373d04f41dc5bae2abecce77fc94e1
Patryk Scheffler <patryk.scheffler@gmail.com>
2017-01-05 18:58:22 +0100

    Check if file exist

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import os
 import sys
+
 
 def main():
     if (len(sys.argv) > 1):
-        print(sys.argv[1])
+        file_path = './resources/' + sys.argv[1]
+        if (os.path.isfile(file_path)):
+            print(file_path)
+        else:
+            print("File doesn't exist")
     else:
         print("You haven't input file name")
 
